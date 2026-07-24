2f991fa026360fbf583390e89c3b7e43a4455880
mani948 <gudalamanikanta5@gmail.com>
2026-07-24 21:38:50 +0530

    Add files via upload

---
+++ towers.py
@@ -0,0 +1,14 @@
+def towerBreakers(n, m):
+    if m == 1:
+        return 2
+    elif n % 2 == 0:
+        return 2
+    else:
+        return 1
+
+
+t = int(input())
+
+for _ in range(t):
+    n, m = map(int, input().split())
+    print(towerBreakers(n, m))
